48c3749a3f37900ef4252d6a2b4eb06f66ce5d2e
SergM2014 <weisse011@gmail.com>
2018-03-02 09:39:03 +0200

    moved to bootstrap4 and navbar added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="csrf-token" content="{{csrf_token()}}">
 
     <title>Laravel</title>
@@ -13,11 +14,12 @@
 </head>
 <body>
 
-    <div  class="container">
+    <div  class="container" id="app">
 
 
           <h1 class="text-center text-danger"> Laravel Master template </h1>
 
+        @include('custom.partials.navigation')
 
         @yield('content')
 
